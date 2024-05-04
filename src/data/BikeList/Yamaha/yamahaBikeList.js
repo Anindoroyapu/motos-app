498294c07c696753ef4cc4b627a9f2bd7b03ea10
Anindo Roy <anindoroy112@gmail.com>
2024-05-04 12:39:11 +0600

    bike_priceList_update

---
+++ src/data/BikeList/Yamaha/yamahaBikeList.js
@@ -0,0 +1,49 @@
+import fzs from "../../../assets/Brand/BikeList/Yamaha/fz-s-right-front-three-quarter-2.webp";
+import fzsfi from "../../../assets/Brand/BikeList/Yamaha/fzs-fi-v4-right-front-three-quarter-3.webp";
+import mt15 from "../../../assets/Brand/BikeList/Yamaha/mt-15-right-front-three-quarter-6.webp";
+import r15 from "../../../assets/Brand/BikeList/Yamaha/r15-right-front-three-quarter-16.webp";
+import fzx from "../../../assets/Brand/BikeList/Yamaha/fz-x-right-front-three-quarter-3.webp";
+
+const yamahaBikeList = [
+  {
+    id: 1,
+    img_url: fzs,
+    title: "FZ-S ",
+    price: "Upcoming..",
+    bikedetails: " ",
+    CC: " ",
+  },
+  {
+    id: 2,
+    img_url: fzsfi,
+    title: "FZS-FI-v4",
+    price: "Upcoming..",
+    bikedetails: " ",
+    CC: " ",
+  },
+  {
+    id: 3,
+    img_url: mt15,
+    title: "MT-15",
+    price: "Upcoming..",
+    bikedetails: " ",
+    CC: " ",
+  },
+  {
+    id: 4,
+    img_url: r15,
+    title: "R15",
+    price: "Upcoming..",
+    bikedetails: " ",
+    CC: " ",
+  },
+  {
+    id: 5,
+    img_url: fzx,
+    title: "FZ-X",
+    price: "Upcoming..",
+    bikedetails: " ",
+    CC: " ",
+  },
+];
+export default yamahaBikeList;
